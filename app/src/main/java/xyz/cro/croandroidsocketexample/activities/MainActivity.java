68845e8af6b35be72733b3c8b0ea03f53f98d5b4
dev.juyoung <dev.juyoung@gmail.com>
2017-09-01 16:13:04 +0900

    * 닉네임 입력 화면 UI 작성.

--- app/src/main/java/xyz/cro/croandroidsocketexample/activities/MainActivity.java
+++ app/src/main/java/xyz/cro/croandroidsocketexample/activities/MainActivity.java
@@ -1,15 +1,68 @@
 package xyz.cro.croandroidsocketexample.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.TextWatcher;
 
+import butterknife.BindColor;
+import butterknife.BindView;
+import butterknife.OnClick;
 import xyz.cro.croandroidsocketexample.R;
+import xyz.cro.croandroidsocketexample.bases.BaseActivity;
+import xyz.cro.croandroidsocketexample.utils.Dlog;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+    @BindView(R.id.usernameInputLayout)
+    TextInputLayout usernameInputLayout;
+    @BindView(R.id.usernameEditText)
+    AppCompatEditText usernameEditText;
+    @BindView(R.id.enteredButton)
+    AppCompatButton enteredButton;
+
+    @BindColor(R.color.textEnabled)
+    int enableTextColor;
+    @BindColor(R.color.textDisabled)
+    int disableTextColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.BrownTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeView();
+    }
+
+    private void initializeView() {
+        getSupportActionBar().setTitle("CroAndroidSocketExample");
+
+        usernameInputLayout.setErrorEnabled(false);
+        usernameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length() > 0) {
+                    enteredButton.setEnabled(true);
+                    enteredButton.setTextColor(enableTextColor);
+                } else {
+                    enteredButton.setEnabled(false);
+                    enteredButton.setTextColor(disableTextColor);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+    }
+
+    @OnClick(R.id.enteredButton)
+    void enteredButtonTouchUp() {
+        String username = usernameEditText.getText().toString();
+        Dlog.d("username: " + username);
     }
 }
--- app/src/main/java/xyz/cro/croandroidsocketexample/bases/BaseActivity.java
+++ app/src/main/java/xyz/cro/croandroidsocketexample/bases/BaseActivity.java
@@ -4,14 +4,20 @@ import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import xyz.cro.croandroidsocketexample.R;
 
 /**
  * Created by juyounglee on 2017. 9. 1..
  */
 
 public class BaseActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar_base)
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +27,12 @@ public class BaseActivity extends AppCompatActivity {
     public void setContentView(@LayoutRes int layoutResID) {
         super.setContentView(layoutResID);
         ButterKnife.bind(this);
+
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
     }
 }
